7fea11599f132e1e4a0c950e17dcb45f7960931f
lairtonmendes <lairton.mendes@gmail.com>
2016-04-25 15:46:03 -0300

    correção erro team class nil

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -11,7 +11,7 @@ class TeamsController < ApplicationController
   # GET /teams/1
   # GET /teams/1.json
   def show
-
+    @team = Team.find(params[:id])
   end
 
   # GET /teams/new
@@ -43,7 +43,6 @@ class TeamsController < ApplicationController
   # PATCH/PUT /teams/1
   # PATCH/PUT /teams/1.json
   def update
-    puts "#{params[:member_id]}"
     respond_to do |format|
       if @team.update(team_params)
         format.html { redirect_to @team, notice: 'Team was successfully updated.' }
